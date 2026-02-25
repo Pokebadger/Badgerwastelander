a5276c56cff8eeac34a49272075caff5ac5b78e4
Mayfrost15 <159665957+Mayfrost15@users.noreply.github.com>
2026-02-26 00:41:09 +0100

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,105 +1,102 @@
 const galleryImages = [
   {
     name: "Annoyed",
-    path: "/assets/materials/Annoyed.jpg",
+    path: "/assets/Materials/gallery/Annoyed.jpg",
     category: "Expression"
   },
   {
     name: "Archery",
-    path: "assets/materials/gallery/Archery.jpg",
+    path: "/assets/Materials/gallery/Archery.jpg",
     category: "Weapon"
   },
   {
     name: "Bar Scene",
-    path: "assets/materials/gallery/Bar_Scene.jpg",
+    path: "/assets/Materials/gallery/Bar_Scene_.jpg",
     category: "Scene"
   },
   {
     name: "BG Galaxy",
-    path: "assets/materials/gallery/BG_Galaxy.jpg",
+    path: "/assets/Materials/gallery/BG_Galaxy.jpg",
     category: "Background"
   },
   {
     name: "BG Nebula",
-    path: "assets/materials/gallery/BG_Nebula_2.jpg",
+    path: "/assets/Materials/gallery/BG_Nebula_2.jpg",
     category: "Background"
   },
   {
     name: "Caesar Eye Patch",
-    path: "assets/materials/gallery/Caesar_Eye_Patch.jpg",
+    path: "/assets/Materials/gallery/Caesar_Eye_Patch.jpg",
     category: "Accessory"
   },
   {
     name: "Cave Ghost",
-    path: "assets/materials/gallery/Cave_Ghost.jpg",
+    path: "/assets/Materials/gallery/Cave_Ghost.jpg",
     category: "Scene"
   },
   {
     name: "Cave Glasses HUD",
-    path: "assets/materials/gallery/Cave_Glasses_HUD.jpg",
+    path: "/assets/Materials/gallery/Cave_Glasses_HUD.jpg",
     category: "Accessory"
   },
   {
     name: "Caesar's Legion",
-    path: "assets/materials/gallery/Ceasars_Legion.jpg",
+    path: "/assets/Materials/gallery/Ceasar_s_Legion.jpg",
     category: "Outfit"
   },
   {
     name: "Cigar Cowboy Hat",
-    path: "assets/materials/gallery/Cigar_Cowboy_Hat.jpg",
+    path: "/assets/Materials/gallery/Cigar_Cowboy_Hat.jpg",
     category: "Accessory"
   },
   {
     name: "Cigar",
-    path: "assets/materials/gallery/Cigar.jpg",
+    path: "/assets/Materials/gallery/Cigar.jpg",
     category: "Accessory"
   },
   {
     name: "Cold",
-    path: "assets/materials/gallery/Cold.jpg",
+    path: "/assets/Materials/gallery/Cold.jpg",
     category: "Scene"
   },
   {
     name: "Excited",
-    path: "assets/materials/gallery/Excited.jpg",
+    path: "/assets/Materials/gallery/Excited.jpg",
     category: "Expression"
   },
   {
     name: "Glasses Sun",
-    path: "assets/materials/gallery/Glasses_Sun.jpg",
+    path: "/assets/Materials/gallery/Glasses_Sun.jpg",
     category: "Accessory"
   },
   {
     name: "Goggles",
-    path: "assets/materials/gallery/Goggles.jpg",
+    path: "/assets/Materials/gallery/Goggles.jpg",
     category: "Accessory"
   },
   {
     name: "Gold",
-    path: "assets/materials/gallery/Gold.jpg",
+    path: "/assets/Materials/gallery/Gold.jpg",
     category: "Trait"
   },
   {
     name: "Gym Swat Gear",
-    path: "assets/materials/gallery/Gym_Swat_Gear_Swat_Helmet.jpg",
+    path: "/assets/Materials/gallery/Gym_Swat_Gear_Swat_Helmet.jpg",
     category: "Outfit"
   },
   {
     name: "Hat Chain",
-    path: "assets/materials/gallery/Hat_Chain.jpg",
+    path: "/assets/Materials/gallery/Hat_Chain.jpg",
     category: "Accessory"
   },
   {
     name: "Metal Badger",
-    path: "assets/materials/gallery/Metal_Badger.jpg",
+    path: "/assets/Materials/gallery/Metal_Badger.jpg",
     category: "Trait"
   },
   {
     name: "Missile Launcher",
-    path: "assets/materials/gallery/Missile_Launcher.jpg",
+    path: "/assets/Materials/gallery/Missile_Launcher.jpg",
     category: "Weapon"
   }
-
 ];
-
-
