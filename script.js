4e38abf0f76c66425701e68f9b554e3942626e18
May A. A. <159665957+Mayfrost15@users.noreply.github.com>
2026-04-22 02:05:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -52,14 +52,10 @@ document.addEventListener('DOMContentLoaded', function() {
       </div>
       <div class="gallery__item-overlay">
         <h3 class="gallery__item-title">${image.name}</h3>
-        <p class="gallery__item-category">${image.category}</p>
         <p class="gallery__item-category">${image.background}</p>
-        <p class="gallery__item-category">${image.armor}</p>
-        <p class="gallery__item-category">${image.face}</p>
+        <p class="gallery__item-category">Armor: ${image.armor}</p>
         <p class="gallery__item-category">${image.head}</p>
         <p class="gallery__item-category">${image.gesture_held}</p>
-        <p class="gallery__item-category">${image.rarity}</p>
-        <p class="gallery__item-category">${image.rank}</p>
       </div>
     `;
     
